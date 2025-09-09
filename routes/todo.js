c798b81e2d999cae77a3311c5130cc56b290ed52
RaditAtalla <radityaderachmadie@gmail.com>
2025-09-09 18:42:16 +0700

    FEAT: get todos

--- routes/todo.js
+++ routes/todo.js
@@ -4,6 +4,29 @@ const authMiddleware = require("../middleware/authMiddleware")
 
 const router = express.Router()
 
-// TODO: Figure out how to store PICs in todo table
+router.get("/:activityId", authMiddleware, async (req, res) => {
+    const token = req.userToken
+    const { activityId } = req.params
+
+    try {
+        const {
+            data: { user },
+        } = await supabase.auth.getUser(token)
+
+        const { data, error } = await supabase
+            .from("todo")
+            .select()
+            .eq("activity_id", activityId)
+            .eq("user_id", user.id)
+
+        if (error) {
+            return res.status(401).send({ success: false, msg: error.message })
+        }
+
+        res.status(200).send({ success: true, todos: data })
+    } catch (error) {
+        res.status(500).send({ success: false, msg: error.message })
+    }
+})
 
 module.exports = router
