e840d4808d79c073f73e862839bdc749d8e63fe7
RaditAtalla <radityaderachmadie@gmail.com>
2025-09-09 17:15:12 +0700

    FEAT; configure routes to the main file

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const cookieParser = require("cookie-parser")
 require("dotenv").config()
 
 const app = express()
-const authRoutes = require("./routes/auth.js")
+const authRoutes = require("./routes/auth")
+const activityRoutes = require("./routes/activity")
+const profileRoutes = require("./routes/profile")
+const notificationRoutes = require("./routes/notification")
+const todoRoutes = require("./routes/todo")
 
 app.use(
     cors({
@@ -19,6 +23,10 @@ app.get("/", (req, res) => {
 })
 
 app.use("/auth", authRoutes)
+app.use("/activity", activityRoutes)
+app.use("/profile", profileRoutes)
+app.use("/notification", notificationRoutes)
+app.use("/todo", todoRoutes)
 
 app.listen(3000, () => {
     console.log("running on http://localhost:3000")
