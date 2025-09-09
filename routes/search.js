418fbdc733462b7874494a171bae178b86902ef3
RaditAtalla <radityaderachmadie@gmail.com>
2025-09-10 00:09:04 +0700

    FEAT: add search user

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,23 @@ const authMiddleware = require("../middleware/authMiddleware")
 
 const router = express.Router()
 
-router.post("/user", async (req, res) => {})
+router.post("/user", async (req, res) => {
+    const { query } = req.query
+
+    try {
+        const { data, error } = await supabase
+            .from("user")
+            .select(`username, fullName, profilePic`)
+            .or(`username.ilike.%${query}%,fullName.ilike.%${query}%}`)
+
+        if (error) {
+            return res.status(400).send({ success: false, msg: error.message })
+        }
+
+        res.status(200).send({ success: true, result: data })
+    } catch (error) {
+        res.status(500).send({ success: false, msg: error.message })
+    }
+})
 
 module.exports = router
